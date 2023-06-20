268ae576d3a085cf138f5b5fb3c91bdcbe0ec969
Dashulkinc <115228373+Dashulkinc@users.noreply.github.com>
2023-06-20 13:10:02 +0300

    Update addrowdialog.cpp

--- addrowdialog.cpp
+++ addrowdialog.cpp
@@ -21,5 +21,13 @@ QStringList AddRowDialog::getData() const
     QStringList newData;
     newData << ui->title->text() << ui->genre->text() << ui->premier->text()
             << ui->runtime->text() << ui->imdb->text() << ui->language->text();
+
+    QDate premierDate = QDate::fromString(ui->premier->text(), "dd.MM.yyyy");
+    if (!premierDate.isValid())
+    {
+        QMessageBox::warning(this, "Ошибка", "Неверный формат даты в поле 'premier'");
+        return QStringList();
+    }
+
     return newData;
 }
